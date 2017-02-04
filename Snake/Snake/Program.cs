bc6217783b6efcaabd8e78977fc531f185e9c26d
Darktarger <jen-96-96@mail.ru>
2017-02-04 19:53:06 +0500

    Создание листов и их использование

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -16,6 +16,24 @@ namespace Snake
             Point p2 = new Point(3,5,'#');
             p2.Draw();
 
+            List<int> NumList = new List<int>(); /* Объявнление списка с целыми числами */
+            NumList.Add(0);
+            NumList.Add(1);
+            NumList.Add(2);
+            int x = NumList[0]; /* Индекс в листе начинается с 0. Х = первому элементу листа */
+            
+            foreach(int i in NumList)    /* Передача всех значений в NumList в переменную i */
+            {
+                Console.WriteLine(i);
+            }
+            NumList.RemoveAt(0); /* Удаление из списка элемента с индексом 0 */
+
+            List<Point> pList = new List<Point>(); /* Список, содержащий точки */
+            pList.Add(p1); /* Помещаем первую точку в лист */
+            pList.Add(p2);
+
+
+
             Console.ReadLine();
         }
     }
